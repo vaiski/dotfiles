5853b83d6daf5ffddde892a713042bd7c50257eb
Eemil Väisänen <eemil.vaisanen@iki.fi>
2023-03-29 20:58:07 +0300

    Remove old Emacs config

--- .emacs.d/projectile.el
+++
@@ -1,8 +0,0 @@
-(require 'projectile)
-
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
-(setq
- projectile-completion-system 'ivy)
-
-(projectile-global-mode +1)
--- .emacs.d/treemacs.el
+++
@@ -1,5 +0,0 @@
-(require 'treemacs)
-
-(define-key global-map (kbd "C-x t t") 'treemacs)
-(define-key global-map (kbd "C-x t d") 'treemacs-select-directory)
-(define-key global-map (kbd "C-x t C-t") 'treemacs-find-file)
